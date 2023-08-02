bf1492e55950e6b4a1eaaf69158cb7e795e28c12
aryansharma16 <aryan.sharma1772002@gmail.com>
2023-08-02 15:37:30 +0530

    jjj

--- src/components/Layout/Spinner.js/Spinner.js
+++ src/components/Layout/Spinner.js/Spinner.js
@@ -1,6 +1,21 @@
-import React from "react";
-
+import React, { useState, useEffect } from "react";
+import { useNavigate, useLocation } from "react-router-dom";
 const Spinner = () => {
+  const [count, setCount] = useState(5); // Updated initial state to 6
+  const navigate = useNavigate();
+const location = useLocation();
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setCount((prevValue) => prevValue - 1); // Updated the decrement logic
+    }, 1000);
+
+    if (count === 1) {
+      navigate("/login", { stat: location.pathname });
+    }
+
+    return () => clearInterval(interval);
+  }, [count, navigate, location]);
+
   return (
     <div className="spinner-container">
       <button className="btn btn-primary" type="button" disabled>
@@ -9,7 +24,7 @@ const Spinner = () => {
           role="status"
           aria-hidden="true"
         ></span>
-        Redirecting to dashboard....
+  &nbsp; &nbsp; Redirecting to you in {count - 1} seconds....{" "}
       </button>
     </div>
   );
