d60bf0e11a26e6f73e36190b6b18cc2585925a2c
aryansharma16 <aryan.sharma1772002@gmail.com>
2023-08-04 17:25:25 +0530

    ff

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -16,17 +16,6 @@ const HomePage = () => {
         <p className="home-page__description">
           Start shopping smarter with SmartCart, the ultimate online shopping platform.
           <pre>{JSON.stringify(auth.token , null, 4)}</pre>
-
-          <div class="dropdown">
-  <button class="btn btn-secondary dropdown-toggle" type="button" data-bs-toggle="dropdown" aria-expanded="false">
-    Dropdown button
-  </button>
-  <ul class="dropdown-menu">
-    <li><a class="dropdown-item" href="#">Action</a></li>
-    <li><a class="dropdown-item" href="#">Another action</a></li>
-    <li><a class="dropdown-item" href="#">Something else here</a></li>
-  </ul>
-</div>
         </p>
       </div>
     </Layout>
